74dc20301a86c64578c7dfb62bf30a8e06b0138f
galales <25105748+galales@users.noreply.github.com>
2021-09-17 11:45:20 +0200

    pin-522: Add status to key api response

--- src/main/scala/it/pagopa/pdnd/interop/uservice/keymanagement/api/impl/KeyApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/keymanagement/api/impl/KeyApiMarshallerImpl.scala
@@ -4,14 +4,14 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.unmarshalling.FromEntityUnmarshaller
 import it.pagopa.pdnd.interop.uservice.keymanagement.api.KeyApiMarshaller
-import it.pagopa.pdnd.interop.uservice.keymanagement.model.{Key, KeySeed, KeysResponse, Problem}
+import it.pagopa.pdnd.interop.uservice.keymanagement.model.{ClientKey, KeySeed, KeysResponse, Problem}
 import spray.json._
 
 class KeyApiMarshallerImpl extends KeyApiMarshaller with SprayJsonSupport with DefaultJsonProtocol {
   override implicit def fromEntityUnmarshallerKeySeedList: FromEntityUnmarshaller[Seq[KeySeed]] =
     sprayJsonUnmarshaller[Seq[KeySeed]]
-  override implicit def toEntityMarshallerKey: ToEntityMarshaller[Key] = sprayJsonMarshaller[Key]
   override implicit def toEntityMarshallerKeysResponse: ToEntityMarshaller[KeysResponse] =
     sprayJsonMarshaller[KeysResponse]
-  override implicit def toEntityMarshallerProblem: ToEntityMarshaller[Problem] = sprayJsonMarshaller[Problem]
+  override implicit def toEntityMarshallerProblem: ToEntityMarshaller[Problem]     = sprayJsonMarshaller[Problem]
+  override implicit def toEntityMarshallerClientKey: ToEntityMarshaller[ClientKey] = sprayJsonMarshaller[ClientKey]
 }
--- src/main/scala/it/pagopa/pdnd/interop/uservice/keymanagement/model/persistence/commands.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/keymanagement/model/persistence/commands.scala
@@ -4,7 +4,7 @@ import akka.Done
 import akka.actor.typed.ActorRef
 import akka.pattern.StatusReply
 import it.pagopa.pdnd.interop.uservice.keymanagement.model.persistence.client.PersistentClient
-import it.pagopa.pdnd.interop.uservice.keymanagement.model.{Key, KeysResponse}
+import it.pagopa.pdnd.interop.uservice.keymanagement.model.{ClientKey, KeysResponse}
 
 import java.util.UUID
 
@@ -12,12 +12,12 @@ sealed trait Command
 
 final case class AddKeys(clientId: String, keys: Seq[ValidKey], replyTo: ActorRef[StatusReply[KeysResponse]])
     extends Command
-final case class GetKeys(clientId: String, replyTo: ActorRef[StatusReply[KeysResponse]])           extends Command
-final case class GetKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[Key]])      extends Command
-final case class DisableKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[Done]]) extends Command
-final case class EnableKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[Done]])  extends Command
-final case class DeleteKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[Done]])  extends Command
-final case class ListKid(from: Int, until: Int, replyTo: ActorRef[StatusReply[Seq[Kid]]])          extends Command
+final case class GetKeys(clientId: String, replyTo: ActorRef[StatusReply[KeysResponse]])            extends Command
+final case class GetKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[ClientKey]]) extends Command
+final case class DisableKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[Done]])  extends Command
+final case class EnableKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[Done]])   extends Command
+final case class DeleteKey(clientId: String, keyId: String, replyTo: ActorRef[StatusReply[Done]])   extends Command
+final case class ListKid(from: Int, until: Int, replyTo: ActorRef[StatusReply[Seq[Kid]]])           extends Command
 
 final case class AddClient(client: PersistentClient, replyTo: ActorRef[StatusReply[PersistentClient]]) extends Command
 final case class GetClient(clientId: String, replyTo: ActorRef[StatusReply[PersistentClient]])         extends Command
