29e8af099b73256e522d5c671848b081d3244dc1
P3trur0 <ubaldopescatore@gmail.com>
2021-10-08 16:21:57 +0200

    added endpoint to retrieve encodedPEM from persistent key

--- src/main/scala/it/pagopa/pdnd/interop/uservice/keymanagement/api/impl/KeyApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/keymanagement/api/impl/KeyApiMarshallerImpl.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.unmarshalling.FromEntityUnmarshaller
 import it.pagopa.pdnd.interop.uservice.keymanagement.api.KeyApiMarshaller
-import it.pagopa.pdnd.interop.uservice.keymanagement.model.{ClientKey, KeySeed, KeysResponse, Problem}
+import it.pagopa.pdnd.interop.uservice.keymanagement.model.{ClientKey, EncodedClientKey, KeySeed, KeysResponse, Problem}
 import spray.json._
 
 class KeyApiMarshallerImpl extends KeyApiMarshaller with SprayJsonSupport with DefaultJsonProtocol {
@@ -14,4 +14,6 @@ class KeyApiMarshallerImpl extends KeyApiMarshaller with SprayJsonSupport with D
     sprayJsonMarshaller[KeysResponse]
   override implicit def toEntityMarshallerProblem: ToEntityMarshaller[Problem]     = sprayJsonMarshaller[Problem]
   override implicit def toEntityMarshallerClientKey: ToEntityMarshaller[ClientKey] = sprayJsonMarshaller[ClientKey]
+  override implicit def toEntityMarshallerEncodedClientKey: ToEntityMarshaller[EncodedClientKey] =
+    sprayJsonMarshaller[EncodedClientKey]
 }
