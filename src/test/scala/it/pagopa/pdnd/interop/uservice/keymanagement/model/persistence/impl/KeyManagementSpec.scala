faa08c2c270644d7f4f558e21e875f49c3294a36
galales <25105748+galales@users.noreply.github.com>
2021-11-08 15:00:20 +0100

    pin-701: Fix value in spec

--- src/test/scala/it/pagopa/pdnd/interop/uservice/keymanagement/model/persistence/impl/KeyManagementSpec.scala
+++ src/test/scala/it/pagopa/pdnd/interop/uservice/keymanagement/model/persistence/impl/KeyManagementSpec.scala
@@ -59,7 +59,7 @@ class KeyManagementSpec
            |  {
            |    "relationshipId": "2ce51cdd-ae78-4829-89cc-39e363270b53",
            |    "key": "LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUF0WGxFTVAwUmEvY0dST050UmliWgppa1FhclUvY2pqaUpDTmNjMFN1dUtYUll2TGRDSkVycEt1UWNSZVhLVzBITGNCd3RibmRXcDhWU25RbkhUY0FpCm9rL0srSzhLblE3K3pEVHlSaTZXY3JhK2dtQi9KanhYeG9ZbjlEbFpBc2tjOGtDYkEvdGNnc1lsL3BmdDJ1YzAKUnNRdEZMbWY3cWVIYzQxa2dpOHNKTjdBbDJuYmVDb3EzWGt0YnBnQkVPcnZxRmttMkNlbG9PKzdPN0l2T3dzeQpjSmFiZ1p2Z01aSm4zeWFMeGxwVGlNanFtQjc5QnJwZENMSHZFaDhqZ2l5djJ2YmdwWE1QTlY1YXhaWmNrTnpRCnhNUWhwRzh5Y2QzaGJrV0s1b2ZkdXMwNEJ0T0c3ejBmbDNnVFp4czdOWDJDVDYzT0RkcnZKSFpwYUlqbks1NVQKbFFJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0t",
-           |    "use": "Sig",
+           |    "use": "SIG",
            |    "alg": "123"
            |  }
            |]
