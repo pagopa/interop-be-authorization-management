8007f1564be14ee6d748a978c578d2a830d21b07
Scala Steward <scala-steward@pagopa.it>
2022-03-23 16:11:27 +0000

    Update akka-actor-testkit-typed, ... to 2.6.19 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   lazy val akkaHttpVersion        = "10.2.6"
   lazy val akkaManagementVersion  = "1.1.1"
-  lazy val akkaVersion            = "2.6.17"
+  lazy val akkaVersion            = "2.6.19"
   lazy val bouncycastleVersion    = "1.70"
   lazy val catsVersion            = "2.7.0"
   lazy val httpJson4sVersion      = "1.38.2"
