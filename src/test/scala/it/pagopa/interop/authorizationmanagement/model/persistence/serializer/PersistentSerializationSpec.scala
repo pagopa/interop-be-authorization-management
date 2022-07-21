1ff8db7efe6f5736895c064c295622668c790f66
Antonio Gelameris <toniogela89@gmail.com>
2022-07-21 12:33:27 +0200

    Added events serde tests

--- src/test/scala/it/pagopa/interop/authorizationmanagement/model/persistence/serializer/PersistentSerializationSpec.scala
+++ src/test/scala/it/pagopa/interop/authorizationmanagement/model/persistence/serializer/PersistentSerializationSpec.scala
@@ -20,6 +20,9 @@ import it.pagopa.interop.authorizationmanagement.model.persistence.serializer._
 import munit.ScalaCheckSuite
 import cats.kernel.Eq
 import PersistentSerializationSpec._
+import it.pagopa.interop.authorizationmanagement.model.persistence.PersistenceTypes._
+import it.pagopa.interop.authorizationmanagement.model.persistence._
+import it.pagopa.interop.authorizationmanagement.model.persistence.serializer.v1.events._
 
 class PersistentSerializationSpec extends ScalaCheckSuite {
 
@@ -29,11 +32,113 @@ class PersistentSerializationSpec extends ScalaCheckSuite {
     }
   }
 
+  property("KeysAdded is correctly deserialized") {
+    forAll(keysAddedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[KeysAddedV1, KeysAdded](stateV1) == Right(state)
+    }
+  }
+  property("KeyDeleted is correctly deserialized") {
+    forAll(keyDeletedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[KeyDeletedV1, KeyDeleted](stateV1) == Right(state)
+    }
+  }
+  property("ClientAdded is correctly deserialized") {
+    forAll(clientAddedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[ClientAddedV1, ClientAdded](stateV1) == Right(state)
+    }
+  }
+  property("ClientDeleted is correctly deserialized") {
+    forAll(clientDeletedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[ClientDeletedV1, ClientDeleted](stateV1) == Right(state)
+    }
+  }
+  property("RelationshipAdded is correctly deserialized") {
+    forAll(relationshipAddedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[RelationshipAddedV1, RelationshipAdded](stateV1) == Right(state)
+    }
+  }
+  property("RelationshipRemoved is correctly deserialized") {
+    forAll(relationshipRemovedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[RelationshipRemovedV1, RelationshipRemoved](stateV1) == Right(state)
+    }
+  }
+  property("ClientPurposeAdded is correctly deserialized") {
+    forAll(clientPurposeAddedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[ClientPurposeAddedV1, ClientPurposeAdded](stateV1) == Right(state)
+    }
+  }
+  property("EServiceStateUpdated is correctly deserialized") {
+    forAll(eServiceStateUpdatedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[EServiceStateUpdatedV1, EServiceStateUpdated](stateV1) == Right(state)
+    }
+  }
+  property("AgreementStateUpdated is correctly deserialized") {
+    forAll(agreementStateUpdatedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[AgreementStateUpdatedV1, AgreementStateUpdated](stateV1) == Right(state)
+    }
+  }
+  property("PurposeStateUpdated is correctly deserialized") {
+    forAll(purposeStateUpdatedGen) { case (state, stateV1) =>
+      PersistEventDeserializer.from[PurposeStateUpdatedV1, PurposeStateUpdated](stateV1) == Right(state)
+    }
+  }
+
   // * Equality has been customized to do not get affected
   // * by different collection kind and order
   property("State is correctly serialized") {
     forAll(stateGenerator) { case (state, stateV1) =>
       PersistEventSerializer.to[State, StateV1](state) === Right(stateV1)
+    }
+  }
+
+  property("KeysAdded is correctly serialized") {
+    forAll(keysAddedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[KeysAdded, KeysAddedV1](state) === Right(stateV1)
+    }
+  }
+  property("KeyDeleted is correctly serialized") {
+    forAll(keyDeletedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[KeyDeleted, KeyDeletedV1](state) == Right(stateV1)
+    }
+  }
+  property("ClientAdded is correctly serialized") {
+    forAll(clientAddedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[ClientAdded, ClientAddedV1](state) == Right(stateV1)
+    }
+  }
+  property("ClientDeleted is correctly serialized") {
+    forAll(clientDeletedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[ClientDeleted, ClientDeletedV1](state) == Right(stateV1)
+    }
+  }
+  property("RelationshipAdded is correctly serialized") {
+    forAll(relationshipAddedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[RelationshipAdded, RelationshipAddedV1](state) == Right(stateV1)
+    }
+  }
+  property("RelationshipRemoved is correctly serialized") {
+    forAll(relationshipRemovedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[RelationshipRemoved, RelationshipRemovedV1](state) == Right(stateV1)
+    }
+  }
+  property("ClientPurposeAdded is correctly serialized") {
+    forAll(clientPurposeAddedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[ClientPurposeAdded, ClientPurposeAddedV1](state) == Right(stateV1)
+    }
+  }
+  property("EServiceStateUpdated is correctly serialized") {
+    forAll(eServiceStateUpdatedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[EServiceStateUpdated, EServiceStateUpdatedV1](state) == Right(stateV1)
+    }
+  }
+  property("AgreementStateUpdated is correctly serialized") {
+    forAll(agreementStateUpdatedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[AgreementStateUpdated, AgreementStateUpdatedV1](state) == Right(stateV1)
+    }
+  }
+  property("PurposeStateUpdated is correctly serialized") {
+    forAll(purposeStateUpdatedGen) { case (state, stateV1) =>
+      PersistEventSerializer.to[PurposeStateUpdated, PurposeStateUpdatedV1](state) == Right(stateV1)
     }
   }
 
@@ -169,40 +274,115 @@ object PersistentSerializationSpec {
     )
   )
 
-  def sizedClientsGenerator(n: Int): Gen[List[((String, PersistentClient), (String, PersistentClientV1))]] =
-    Gen.containerOfN[List, (PersistentClient, PersistentClientV1)](n, persistentClientGen).map { list =>
-      list.map { case (pc, pcv1) => (pc.id.toString -> pc, pcv1.id -> pcv1) }
+  def sizedClientsGenerator(n: Int): Gen[(Map[String, PersistentClient], List[StateClientsEntryV1])] =
+    Gen.containerOfN[List, (PersistentClient, PersistentClientV1)](n, persistentClientGen).map {
+      _.foldLeft((Map.empty[String, PersistentClient], List.empty[StateClientsEntryV1])) {
+        case ((map, list), (pc, pcv1)) => (map + (pc.id.toString -> pc), StateClientsEntryV1(pcv1.id, pcv1) :: list)
+      }
     }
 
-  def sizedKeysGenerator(n: Int): Gen[(Map[String, PersistentKey], Map[String, PersistentKeyV1])] =
+  def sizedKeysGenerator(n: Int): Gen[(Keys, List[PersistentKeyEntryV1])] = Gen
+    .containerOfN[List, (PersistentKey, PersistentKeyV1)](n, persistentKeyGen)
+    .map(_.foldLeft((Map.empty[String, PersistentKey], List.empty[PersistentKeyEntryV1])) {
+      case ((map, list), (pk, pkv1)) => (map + (pk.kid -> pk), PersistentKeyEntryV1(pkv1.kid, pkv1) :: list)
+    })
+
+  def keysGen(clientsIds: List[String], cardinality: Int): Gen[(Map[String, Keys], List[StateKeysEntryV1])] =
     Gen
-      .containerOfN[List, ((String, PersistentKey), (String, PersistentKeyV1))](
-        n,
-        persistentKeyGen.map { case (pk, pkv1) => (pk.kid -> pk, pkv1.kid -> pkv1) }
+      .sequence(
+        clientsIds.map(id => sizedKeysGenerator(cardinality).map { case (keys, keysv1) => ((id, keys), (id, keysv1)) })
       )
-      .map { list =>
-        val (pKeys, pKeysV1) = list.separate
-        (pKeys.toMap, pKeysV1.toMap)
-      }
+      .map(_.asScala.toList.foldLeft((Map.empty[String, Keys], List.empty[StateKeysEntryV1])) {
+        case ((map, list), (keys, keysV1)) => (map + keys, StateKeysEntryV1(keysV1._1, keysV1._2) :: list)
+      })
 
   val stateGenerator: Gen[(State, StateV1)] = for {
-    cardinality    <- Gen.chooseNum(1, 10)
-    mapCardinality <- Gen.chooseNum(1, 2)
-    clientsList    <- sizedClientsGenerator(cardinality)
-    (clients, clientsV1)       = clientsList.separate
-    (clientsMap, clientsV1Map) = (clients.toMap, clientsV1.map { case (k, v) => StateClientsEntryV1(k, v) }.toSeq)
-    (keys, keysv1) <- Gen
-      .sequence(clientsList.map { case ((pcid, _), (pcv1id, _)) =>
-        sizedKeysGenerator(mapCardinality).map { case (keys, keysv1) => ((pcid, keys), (pcv1id, keysv1)) }
-      })
-      .map { list =>
-        val (a, b) = list.asScala.toList.separate
-        (
-          a.toMap,
-          b.map { case (k, v) => StateKeysEntryV1(k, v.map { case (x, y) => PersistentKeyEntryV1(x, y) }.toSeq) }
-        )
-      }
+    cardinality                <- Gen.chooseNum(1, 10)
+    mapCardinality             <- Gen.chooseNum(1, 2)
+    (clientsMap, clientsV1Map) <- sizedClientsGenerator(cardinality)
+    (keys, keysv1)             <- keysGen(clientsMap.keySet.toList, mapCardinality)
   } yield (State(keys, clientsMap), StateV1(keysv1, clientsV1Map))
+
+  val keysAddedGen: Gen[(KeysAdded, KeysAddedV1)] = for {
+    clientId         <- stringGen
+    cardinality      <- Gen.chooseNum(1, 10)
+    (pKeys, pKeysV1) <- sizedKeysGenerator(cardinality)
+  } yield (KeysAdded(clientId, pKeys), KeysAddedV1(clientId, pKeysV1))
+
+  val keyDeletedGen: Gen[(KeyDeleted, KeyDeletedV1)] = for {
+    clientId      <- stringGen
+    keyId         <- stringGen
+    (time, times) <- offsetDatetimeGen
+  } yield (KeyDeleted(clientId, keyId, time), KeyDeletedV1(clientId, keyId, times))
+
+  val clientAddedGen: Gen[(ClientAdded, ClientAddedV1)] = persistentClientGen.map { case (pc, pcv1) =>
+    (ClientAdded(pc), ClientAddedV1(pcv1))
+  }
+
+  val clientDeletedGen: Gen[(ClientDeleted, ClientDeletedV1)] =
+    stringGen.map(s => (ClientDeleted(s), ClientDeletedV1(s)))
+
+  val relationshipAddedGen: Gen[(RelationshipAdded, RelationshipAddedV1)] = for {
+    (pc, pcv1) <- persistentClientGen
+    uuid       <- Gen.uuid
+  } yield (RelationshipAdded(pc, uuid), RelationshipAddedV1(pcv1, uuid.toString()))
+
+  val relationshipRemovedGen: Gen[(RelationshipRemoved, RelationshipRemovedV1)] = for {
+    clientId       <- stringGen
+    relationshipId <- stringGen
+  } yield (RelationshipRemoved(clientId, relationshipId), RelationshipRemovedV1(clientId, relationshipId))
+
+  val clientPurposeAddedGen: Gen[(ClientPurposeAdded, ClientPurposeAddedV1)] = for {
+    clientId         <- stringGen
+    (chain, chainV1) <- persistentClientStatesChainGen
+  } yield (ClientPurposeAdded(clientId, chain), ClientPurposeAddedV1(clientId, chainV1))
+
+  val eServiceStateUpdatedGen: Gen[(EServiceStateUpdated, EServiceStateUpdatedV1)] = for {
+    eServiceId       <- stringGen
+    descriptorId     <- Gen.uuid
+    (state, stateV1) <- persistentClientComponentStateGen
+    audiences        <- Gen.listOf(stringGen)
+    voucherLifespan  <- Gen.posNum[Int]
+  } yield (
+    EServiceStateUpdated(
+      eServiceId = eServiceId,
+      descriptorId = descriptorId,
+      state = state,
+      audience = audiences,
+      voucherLifespan = voucherLifespan
+    ),
+    EServiceStateUpdatedV1(
+      eServiceId = eServiceId,
+      descriptorId = descriptorId.toString(),
+      state = stateV1,
+      audience = audiences,
+      voucherLifespan = voucherLifespan
+    )
+  )
+
+  val agreementStateUpdatedGen: Gen[(AgreementStateUpdated, AgreementStateUpdatedV1)] = for {
+    eServiceId       <- stringGen
+    consumerId       <- stringGen
+    agreementId      <- Gen.uuid
+    (state, stateV1) <- persistentClientComponentStateGen
+  } yield (
+    AgreementStateUpdated(eServiceId = eServiceId, consumerId = consumerId, agreementId = agreementId, state = state),
+    AgreementStateUpdatedV1(
+      eServiceId = eServiceId,
+      consumerId = consumerId,
+      agreementId = agreementId.toString(),
+      state = stateV1
+    )
+  )
+
+  val purposeStateUpdatedGen: Gen[(PurposeStateUpdated, PurposeStateUpdatedV1)] = for {
+    purposeId        <- stringGen
+    versionId        <- Gen.uuid
+    (state, stateV1) <- persistentClientComponentStateGen
+  } yield (
+    PurposeStateUpdated(purposeId = purposeId, versionId = versionId, state = state),
+    PurposeStateUpdatedV1(purposeId = purposeId, versionId = versionId.toString, state = stateV1)
+  )
 
   implicit val persistenClientEq: Eq[PersistentClientV1] = Eq.instance { case (result, expected) =>
     result.consumerId === expected.consumerId &&
@@ -218,7 +398,13 @@ object PersistentSerializationSpec {
 
   implicit val stateV1Equality: Eq[StateV1] = Eq.instance { case (result, expected) =>
     val keysEquality: Boolean =
-      result.keys.toList.sortBy(_.clientId) == expected.keys.toList.sortBy(_.clientId)
+      result.keys
+        .map(s => StateKeysEntryV1(s.clientId, s.keyEntries.sortBy(_.keyId)))
+        .toList
+        .sortBy(_.clientId) == expected.keys
+        .map(s => StateKeysEntryV1(s.clientId, s.keyEntries.sortBy(_.keyId)))
+        .toList
+        .sortBy(_.clientId)
 
     val clientsEquality: Boolean =
       result.clients.toList.sortBy(_.clientId).zip(expected.clients.toList.sortBy(_.clientId)).forall { case (a, b) =>
@@ -226,6 +412,10 @@ object PersistentSerializationSpec {
       }
 
     keysEquality && clientsEquality
+  }
+
+  implicit val keysAddedV1Eq: Eq[KeysAddedV1] = Eq.instance { case (a, b) =>
+    a.clientId == b.clientId && a.keys.sortBy(_.keyId) == b.keys.sortBy(_.keyId)
   }
 
   implicit val throwableEq: Eq[Throwable] = Eq.fromUniversalEquals
