86a80a791189e7398f4f02b78fde5646084ec511
Antonio Gelameris <toniogela89@gmail.com>
2022-12-23 12:48:34 +0100

    Update Dependencies.scala

--- src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Dependencies.scala
+++ src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Dependencies.scala
@@ -45,7 +45,7 @@ import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
 import scala.concurrent.{ExecutionContext, Future}
-import com.typesafe.scalalogging.{Logger, LoggerTakinImplicit}
+import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
 import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 
 trait Dependencies {
