4de615ccfd1cb5a17c4a1f376d3d1658a02648cf
AsterITA <fabrizio.musella@gmail.com>
2023-05-02 09:43:00 +0200

    [Pin 3281] - Added projection for collection keys (#206)

--- src/main/scala/it/pagopa/interop/authorizationmanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/authorizationmanagement/common/system/ApplicationConfiguration.scala
@@ -19,14 +19,15 @@ object ApplicationConfiguration {
   def projectionTag(index: Int) = s"interop-be-authorization-management-persistence|$index"
 
   // Loaded only if projections are enabled
-  lazy val mongoDb: MongoDbConfig = {
+  lazy val (clientsMongoDB, keysMongoDB): (MongoDbConfig, MongoDbConfig) = {
     val connectionString: String = config.getString("cqrs-projection.db.connection-string")
     val dbName: String           = config.getString("cqrs-projection.db.name")
-    val collectionName: String   = config.getString("cqrs-projection.db.collection-name")
 
-    MongoDbConfig(connectionString, dbName, collectionName)
+    (
+      MongoDbConfig(connectionString, dbName, config.getString("cqrs-projection.db.clients-collection-name")),
+      MongoDbConfig(connectionString, dbName, config.getString("cqrs-projection.db.keys-collection-name"))
+    )
   }
 
   require(jwtAudience.nonEmpty, "Audience cannot be empty")
-
 }
--- src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Dependencies.scala
+++ src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Dependencies.scala
@@ -12,6 +12,7 @@ import akka.projection.ProjectionBehavior
 import com.atlassian.oai.validator.report.ValidationReport
 import com.nimbusds.jose.proc.SecurityContext
 import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier
+import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
 import it.pagopa.interop.authorizationmanagement.api._
 import it.pagopa.interop.authorizationmanagement.api.impl.{
   ClientApiMarshallerImpl,
@@ -31,11 +32,12 @@ import it.pagopa.interop.authorizationmanagement.common.system.ApplicationConfig
   numberOfProjectionTags,
   projectionTag
 }
-import it.pagopa.interop.authorizationmanagement.model.persistence.projection.ClientCqrsProjection
+import it.pagopa.interop.authorizationmanagement.model.persistence.projection.{ClientCqrsProjection, KeyCqrsProjection}
 import it.pagopa.interop.authorizationmanagement.model.persistence.{Command, KeyPersistentBehavior}
 import it.pagopa.interop.commons.jwt.service.JWTReader
 import it.pagopa.interop.commons.jwt.service.impl.{DefaultJWTReader, getClaimsVerifier}
 import it.pagopa.interop.commons.jwt.{JWTConfiguration, KID, PublicKeysHolder, SerializedKey}
+import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 import it.pagopa.interop.commons.utils.AkkaUtils.PassThroughAuthenticator
 import it.pagopa.interop.commons.utils.OpenapiUtils
 import it.pagopa.interop.commons.utils.TypeConversions._
@@ -45,8 +47,6 @@ import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
 import scala.concurrent.{ExecutionContext, Future}
-import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
-import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 
 trait Dependencies {
 
@@ -77,13 +77,32 @@ trait Dependencies {
   }
 
   def initProjections()(implicit actorSystem: ActorSystem[_], ec: ExecutionContext): Unit = {
+    initClientCqrsProjection()
+    initKeyCqrsProjection()
+  }
+
+  def initClientCqrsProjection()(implicit actorSystem: ActorSystem[_], ec: ExecutionContext): Unit = {
     val dbConfig: DatabaseConfig[JdbcProfile] =
       DatabaseConfig.forConfig("akka-persistence-jdbc.shared-databases.slick")
 
-    val mongoDbConfig = ApplicationConfiguration.mongoDb
-
     val projectionId   = "client-cqrs-projections"
-    val cqrsProjection = ClientCqrsProjection.projection(dbConfig, mongoDbConfig, projectionId)
+    val cqrsProjection =
+      ClientCqrsProjection.projection(dbConfig, ApplicationConfiguration.clientsMongoDB, projectionId)
+
+    ShardedDaemonProcess(actorSystem).init[ProjectionBehavior.Command](
+      name = projectionId,
+      numberOfInstances = numberOfProjectionTags,
+      behaviorFactory = (i: Int) => ProjectionBehavior(cqrsProjection.projection(projectionTag(i))),
+      stopMessage = ProjectionBehavior.Stop
+    )
+  }
+
+  def initKeyCqrsProjection()(implicit actorSystem: ActorSystem[_], ec: ExecutionContext): Unit = {
+    val dbConfig: DatabaseConfig[JdbcProfile] =
+      DatabaseConfig.forConfig("akka-persistence-jdbc.shared-databases.slick")
+
+    val projectionId   = "key-cqrs-projections"
+    val cqrsProjection = KeyCqrsProjection.projection(dbConfig, ApplicationConfiguration.keysMongoDB, projectionId)
 
     ShardedDaemonProcess(actorSystem).init[ProjectionBehavior.Command](
       name = projectionId,
