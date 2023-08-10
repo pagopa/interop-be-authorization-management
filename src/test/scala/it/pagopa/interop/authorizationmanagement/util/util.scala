258e56e54158b0d2accb4f10a5fc878add742ea2
Fabrizio Musella <fabrizio.musella@pagopa.it>
2023-08-10 12:03:20 +0200

    removed unused file

--- src/test/scala/it/pagopa/interop/authorizationmanagement/util/util.scala
+++
@@ -1,9 +0,0 @@
-package it.pagopa.interop.authorizationmanagement
-
-import it.pagopa.interop.commons.jwt._
-
-package object util {
-
-  final val existingRoles: Seq[String] = Seq(ADMIN_ROLE, SECURITY_ROLE, API_ROLE, M2M_ROLE, INTERNAL_ROLE)
-
-}
