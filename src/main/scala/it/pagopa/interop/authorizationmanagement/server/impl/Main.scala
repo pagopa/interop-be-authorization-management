0a0220454349df98b76ddf93363cbc61cd7bc12f
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-08-02 17:04:37 +0200

    remove projections (#224)

--- src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Main.scala
+++ src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Main.scala
@@ -1,9 +1,7 @@
 package it.pagopa.interop.authorizationmanagement.server.impl
 
-import cats.syntax.all._
-import buildinfo.BuildInfo
-import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
 import akka.cluster.ClusterEvent
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.cluster.typed.{Cluster, Subscribe}
@@ -11,15 +9,15 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.management.cluster.bootstrap.ClusterBootstrap
 import akka.management.scaladsl.AkkaManagement
+import buildinfo.BuildInfo
+import cats.syntax.all._
+import com.typesafe.scalalogging.Logger
 import it.pagopa.interop.authorizationmanagement.common.system.ApplicationConfiguration
 import it.pagopa.interop.authorizationmanagement.server.Controller
 import it.pagopa.interop.commons.logging.renderBuildInfo
-import scala.concurrent.ExecutionContext
-import com.typesafe.scalalogging.Logger
-import scala.concurrent.Future
-import scala.util.{Success, Failure}
-import akka.actor.typed.DispatcherSelector
-import scala.concurrent.ExecutionContextExecutor
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object Main extends App with Dependencies {
 
@@ -30,8 +28,6 @@ object Main extends App with Dependencies {
       implicit val actorSystem: ActorSystem[_]        = context.system
       implicit val executionContext: ExecutionContext = actorSystem.executionContext
       // Let's keep it here in the case we'll need to call any external service
-      val selector: DispatcherSelector                = DispatcherSelector.fromConfig("futures-dispatcher")
-      val blockingEc: ExecutionContextExecutor        = actorSystem.dispatchers.lookup(selector)
 
       AkkaManagement.get(actorSystem).start()
 
@@ -50,8 +46,6 @@ object Main extends App with Dependencies {
       )
 
       cluster.subscriptions ! Subscribe(listener, classOf[ClusterEvent.MemberEvent])
-
-      if (ApplicationConfiguration.projectionsEnabled) initProjections(blockingEc)
 
       logger.info(renderBuildInfo(BuildInfo))
       logger.info(s"Started cluster at ${cluster.selfMember.address}")
