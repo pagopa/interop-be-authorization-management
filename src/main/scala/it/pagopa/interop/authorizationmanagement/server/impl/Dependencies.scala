7fb3a97ac14633b11c50dba93f8f2c246beadad7
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-08-02 16:55:28 +0200

    remove projections

--- src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Dependencies.scala
+++ src/main/scala/it/pagopa/interop/authorizationmanagement/server/impl/Dependencies.scala
@@ -2,61 +2,49 @@ package it.pagopa.interop.authorizationmanagement.server.impl
 
 import akka.actor.typed.{ActorSystem, Behavior}
 import akka.cluster.sharding.typed.ShardingEnvelope
-import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity, EntityContext, ShardedDaemonProcess}
+import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity, EntityContext}
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.SecurityDirectives
 import akka.persistence.typed.PersistenceId
-import akka.projection.ProjectionBehavior
 import com.atlassian.oai.validator.report.ValidationReport
 import com.nimbusds.jose.proc.SecurityContext
 import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier
 import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
 import it.pagopa.interop.authorizationmanagement.api._
 import it.pagopa.interop.authorizationmanagement.api.impl.{
-  MigrateApiMarshallerImpl,
   ClientApiMarshallerImpl,
   ClientApiServiceImpl,
   HealthApiMarshallerImpl,
   HealthServiceApiImpl,
   KeyApiMarshallerImpl,
+  KeyApiServiceImpl,
+  MigrateApiMarshallerImpl,
+  MigrateApiServiceImpl,
   PurposeApiMarshallerImpl,
   PurposeApiServiceImpl,
   TokenGenerationApiMarshallerImpl,
   TokenGenerationApiServiceImpl,
   serviceCode
 }
-import it.pagopa.interop.authorizationmanagement.api.impl.{MigrateApiServiceImpl, KeyApiServiceImpl}
 import it.pagopa.interop.authorizationmanagement.common.system.ApplicationConfiguration
 import it.pagopa.interop.authorizationmanagement.common.system.ApplicationConfiguration.{
   numberOfProjectionTags,
   projectionTag
 }
-import it.pagopa.interop.authorizationmanagement.model.persistence.projection.{
-  ClientCqrsProjection,
-  ClientNotificationProjection,
-  KeyCqrsProjection
-}
-import it.pagopa.interop.authorizationmanagement.model.persistence.{
-  AuthorizationEventsSerde,
-  Command,
-  KeyPersistentBehavior
-}
+import it.pagopa.interop.authorizationmanagement.model.persistence.{Command, KeyPersistentBehavior}
 import it.pagopa.interop.commons.jwt.service.JWTReader
 import it.pagopa.interop.commons.jwt.service.impl.{DefaultJWTReader, getClaimsVerifier}
 import it.pagopa.interop.commons.jwt.{JWTConfiguration, KID, PublicKeysHolder, SerializedKey}
 import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
-import it.pagopa.interop.commons.queue.QueueWriter
 import it.pagopa.interop.commons.utils.AkkaUtils.PassThroughAuthenticator
 import it.pagopa.interop.commons.utils.OpenapiUtils
 import it.pagopa.interop.commons.utils.TypeConversions._
 import it.pagopa.interop.commons.utils.errors.{Problem => CommonProblem}
 import it.pagopa.interop.commons.utils.service.{OffsetDateTimeSupplier, UUIDSupplier}
-import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
 
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.concurrent.{ExecutionContext, Future}
 
 trait Dependencies {
 
@@ -85,67 +73,6 @@ trait Dependencies {
     val error =
       CommonProblem(StatusCodes.BadRequest, OpenapiUtils.errorFromRequestValidationReport(report), serviceCode, None)
     complete(error.status, error)
-  }
-
-  def initProjections(
-    blockingEc: ExecutionContextExecutor
-  )(implicit actorSystem: ActorSystem[_], ec: ExecutionContext): Unit = {
-    initClientCqrsProjection()
-    initKeyCqrsProjection()
-    initNotificationProjection(blockingEc)
-  }
-
-  def initClientCqrsProjection()(implicit actorSystem: ActorSystem[_], ec: ExecutionContext): Unit = {
-    val dbConfig: DatabaseConfig[JdbcProfile] =
-      DatabaseConfig.forConfig("akka-persistence-jdbc.shared-databases.slick")
-
-    val projectionId   = "client-cqrs-projections"
-    val cqrsProjection =
-      ClientCqrsProjection.projection(dbConfig, ApplicationConfiguration.clientsMongoDB, projectionId)
-
-    ShardedDaemonProcess(actorSystem).init[ProjectionBehavior.Command](
-      name = projectionId,
-      numberOfInstances = numberOfProjectionTags,
-      behaviorFactory = (i: Int) => ProjectionBehavior(cqrsProjection.projection(projectionTag(i))),
-      stopMessage = ProjectionBehavior.Stop
-    )
-  }
-
-  def initKeyCqrsProjection()(implicit actorSystem: ActorSystem[_], ec: ExecutionContext): Unit = {
-    val dbConfig: DatabaseConfig[JdbcProfile] =
-      DatabaseConfig.forConfig("akka-persistence-jdbc.shared-databases.slick")
-
-    val projectionId   = "key-cqrs-projections"
-    val cqrsProjection = KeyCqrsProjection.projection(dbConfig, ApplicationConfiguration.keysMongoDB, projectionId)
-
-    ShardedDaemonProcess(actorSystem).init[ProjectionBehavior.Command](
-      name = projectionId,
-      numberOfInstances = numberOfProjectionTags,
-      behaviorFactory = (i: Int) => ProjectionBehavior(cqrsProjection.projection(projectionTag(i))),
-      stopMessage = ProjectionBehavior.Stop
-    )
-  }
-
-  def initNotificationProjection(
-    blockingEc: ExecutionContextExecutor
-  )(implicit actorSystem: ActorSystem[_], ec: ExecutionContext): Unit = {
-    val queueWriter: QueueWriter =
-      QueueWriter.get(ApplicationConfiguration.queueUrl)(AuthorizationEventsSerde.authToJson)(blockingEc)
-
-    val dbConfig: DatabaseConfig[JdbcProfile] =
-      DatabaseConfig.forConfig("akka-persistence-jdbc.shared-databases.slick")
-
-    val notificationProjectionId: String = "authorization-notification-projections"
-
-    val clientNotificationProjection: ClientNotificationProjection =
-      new ClientNotificationProjection(dbConfig, queueWriter, notificationProjectionId)
-
-    ShardedDaemonProcess(actorSystem).init[ProjectionBehavior.Command](
-      name = notificationProjectionId,
-      numberOfInstances = numberOfProjectionTags,
-      behaviorFactory = (i: Int) => ProjectionBehavior(clientNotificationProjection.projection(projectionTag(i))),
-      stopMessage = ProjectionBehavior.Stop
-    )
   }
 
   def migrateApi(jwtReader: JWTReader, sharding: ClusterSharding)(implicit actorSystem: ActorSystem[_]) =
